b60b3882c1f4bf8dff57cd66f45e70286404616e
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-06-01 16:56:26 -0300

    rev

--- src/main.c
+++ src/main.c
@@ -13,31 +13,32 @@ static void enter_account(struct netmsg netmsg)
 	rsa_decrypt(netmsg.buf + 17);
 
 	const uint32_t account_number = memread_u32(netmsg.buf + 34);
+
 	const uint16_t account_password_len = memread_u16(netmsg.buf + 38);
 	char account_password[account_password_len + 1];
 	memcpy(account_password, netmsg.buf + 40, account_password_len);
 	account_password[account_password_len] = '\0';
-	char ipaddr[COTS_IP_ADDR_BUFFER_SIZE];
-	connection_get_ip_addr(netmsg.conn_info, ipaddr);
 
-	log_debug("Login attempt from IP Address: %s\n"
-		      "Account Number: %" PRIu32 "\n"
+	log_debug("Login attempt:\n"
+	          "Account Number: %" PRIu32 "\n"
 	          "Account Password: %s\n",
-	          ipaddr, account_number, account_password);
+	           account_number, account_password);
 
 }
 
 static void login_protocol_handler(struct netmsg netmsg)
 {
+
+	char ip_addr[COTS_IP_ADDR_BUFFER_SIZE];
+	connection_get_ip_addr(netmsg.conn_info, ip_addr);
 	const uint8_t opcode = memread_u8(netmsg.buf);
 
-	log_debug("New Request From: \n"
+	log_debug("New Request From: %s\n"
 	          "Protocol Opcode: %" PRIu8 "\n"
 	          "Client OS: %" PRIu16 "\n"
 	          "Client Version: %" PRIu16 "\n",
-	          opcode,
-	          memread_u16(netmsg.buf + 1),
-	          memread_u16(netmsg.buf + 3));
+	          ip_addr, opcode,
+	          memread_u16(netmsg.buf + 1), memread_u16(netmsg.buf + 3));
 
 	switch (opcode) {
 	case COTS_OPCODE_ENTER_ACCOUNT:
