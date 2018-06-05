e80e09932f2362fd536305eefe0b13f0c9548639
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-06-05 19:33:27 -0300

    xtea delta contant

--- src/xtea.c
+++ src/xtea.c
@@ -1,6 +1,9 @@
 #include "xtea.h"
 
 
+#define XTEA_DELTA (0x61C88647)
+
+
 void xtea_encrypt(const uint32_t key[4], uint8_t* const data, const uint16_t len)
 {
 	const uint32_t* const k = key;
@@ -10,12 +13,11 @@ void xtea_encrypt(const uint32_t key[4], uint8_t* const data, const uint16_t len
 
 	while (read_pos < (len / 4)) {
 		uint32_t v0 = buffer[read_pos], v1 = buffer[read_pos + 1];
-		uint32_t delta = 0x61C88647;
 	 	uint32_t sum = 0;
 
 		for (int32_t i = 0; i < 32; ++i) {
 			v0 += ((v1 << 4 ^ v1 >> 5) + v1) ^ (sum + k[sum & 3]);
-			sum -= delta;
+			sum -= XTEA_DELTA;
 			v1 += ((v0 << 4 ^ v0 >> 5) + v0) ^ (sum + k[sum>>11 & 3]);
 		}
 
@@ -33,12 +35,11 @@ void xtea_decrypt(const uint32_t key[4], uint8_t* const data, const uint16_t len
 	
 	while (read_pos < (len / 4)) {
 		uint32_t v0 = buffer[read_pos], v1 = buffer[read_pos + 1];
-		uint32_t delta = 0x61C88647;
 		uint32_t sum = 0xC6EF3720;
 
 		for (int32_t i = 0; i < 32; i++) {
 			v1 -= ((v0 << 4 ^ v0 >> 5) + v0) ^ (sum + k[sum>>11 & 3]);
-			sum += delta;
+			sum += XTEA_DELTA;
 			v0 -= ((v1 << 4 ^ v1 >> 5) + v1) ^ (sum + k[sum & 3]);
 		}
 
