c7cefe4b7f63a825a4bb2c08d0e8f594a4f46c0f
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-06-05 20:44:43 -0300

    review buffer data

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@ static void signal_handler(int sig)
 	signal_recv = true;
 }
 
-static void enter_account(struct conn_info* const ci)
+static void opcode_enter_account(struct conn_info* const ci)
 {
 	ci->xtea_key[0] = memread_u32(ci->in_nm.buf + 18);
 	ci->xtea_key[1] = memread_u32(ci->in_nm.buf + 22);
@@ -34,6 +34,9 @@ static void enter_account(struct conn_info* const ci)
 	         ci->xtea_key[2], ci->xtea_key[3]); 
 
 
+	const uint16_t client_os = memread_u16(ci->in_nm.buf + 1);
+	const uint16_t client_version = memread_u16(ci->in_nm.buf + 3);
+
 	const uint32_t account_number = memread_u32(ci->in_nm.buf + 34);
 
 	const uint16_t account_password_len = memread_u16(ci->in_nm.buf + 38);
@@ -42,8 +45,11 @@ static void enter_account(struct conn_info* const ci)
 	account_password[account_password_len] = '\0';
 
 	log_debug("Login attempt:\n"
+	          "Client OS: %" PRIu16 "\n"
+		  "Client Version: %" PRIu16 "\n"
 	          "Account Number: %" PRIu32 "\n"
 	          "Account Password: %s\n",
+		   client_os, client_version,
 	           account_number, account_password);
 
 	const char* const test_msg = "Server Response: In Maintenance.";
@@ -57,22 +63,16 @@ static void enter_account(struct conn_info* const ci)
 
 static void login_protocol_handler(struct conn_info* const ci)
 {
-
 	char ip_addr[COTS_IP_ADDR_BUFFER_SIZE];
 	connection_get_ip_addr(ci, ip_addr);
 	const uint8_t opcode = memread_u8(ci->in_nm.buf);
 
-	log_debug("New Request From: %s\n"
-	          "Protocol Opcode: %" PRIu8 "\n"
-	          "Client OS: %" PRIu16 "\n"
-	          "Client Version: %" PRIu16 "\n",
-	          ip_addr, opcode,
-	          memread_u16(ci->in_nm.buf + 1), memread_u16(ci->in_nm.buf + 3));
+	log_debug("New Request From: %s to login protocol\n"
+	          "Protocol Opcode: %" PRIu8 "\n",
+	          ip_addr, opcode);
 
 	switch (opcode) {
-	case COTS_OPCODE_ENTER_ACCOUNT:
-		enter_account(ci);
-		break;
+	case COTS_OPCODE_ENTER_ACCOUNT: opcode_enter_account(ci); break;
 	}
 
 }
