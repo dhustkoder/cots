41f1089a4fa1c6b73527aafde3f2f8f28ec3a98e
Rafael Moura <rafaelmoura.dev@gmail.com>
2018-07-16 13:24:21 -0300

    revision / free mpz / performance

--- src/rsa.c
+++ src/rsa.c
@@ -8,16 +8,26 @@ const char* const p_str = "14299623962416399520070177382898895550795403345466153
 const char* const q_str = "7630979195970404721891201847792002125535401292779123937207447574596692788513647179235335529307251350570728407373705564708871762033017096809910315212884101";
 const char* const d_str = "46730330223584118622160180015036832148732986808519344675210555262940258739805766860224610646919605860206328024326703361630109888417839241959507572247284807035235569619173792292786907845791904955103601652822519121908367187885509270025388641700821735345222087940578381210879116823013776808975766851829020659073";
 static mpz_t p, q, d, u, mod, dp, dq;
+static mpz_t c, v1, v2, u2, tmp;
 
 
 void rsa_init(void)
 {
 	log_info("Initializing RSA...");
+
 	mpz_set_str(p, p_str, 10);
 	mpz_set_str(q, q_str, 10);
 	mpz_set_str(d, d_str, 10);
 
-	mpz_t pm1,qm1;
+	mpz_init2(c, 1024);
+	mpz_init2(v1, 1024);
+	mpz_init2(v2, 1024);
+	mpz_init2(u2, 1024);
+	mpz_init2(tmp, 1024);
+
+
+	mpz_t pm1, qm1;
+
 	mpz_init2(pm1, 520);
 	mpz_init2(qm1, 520);
 
@@ -36,16 +46,24 @@ void rsa_init(void)
 void rsa_term(void)
 {
 	log_info("Terminating RSA...");
+	
+	mpz_clear(tmp);
+	mpz_clear(u2);
+	mpz_clear(v2);
+	mpz_clear(v1);
+	mpz_clear(c);
+
+	mpz_clear(mod);
+	mpz_clear(dq);
+	mpz_clear(dp);
+	mpz_clear(u);
+	mpz_clear(d);
+	mpz_clear(q);
+	mpz_clear(p);
 }
 
 void rsa_decrypt(uint8_t buffer[128])
 {
-	mpz_t c,v1,v2,u2,tmp;
-	mpz_init2(c, 1024);
-	mpz_init2(v1, 1024);
-	mpz_init2(v2, 1024);
-	mpz_init2(u2, 1024);
-	mpz_init2(tmp, 1024);
 
 	mpz_import(c, 128, 1, 1, 0, 0, buffer);
 
@@ -69,13 +87,4 @@ void rsa_decrypt(uint8_t buffer[128])
 	size_t count = (mpz_sizeinbase(c, 2) + 7)/8;
 	memset(buffer, 0, 128 - count);
 	mpz_export(&buffer[128 - count], NULL, 1, 1, 0, 0, c);
-
-	mpz_clear(c);
-	mpz_clear(v1);
-	mpz_clear(v2);
-	mpz_clear(u2);
-	mpz_clear(tmp);
 }
-
-
-
